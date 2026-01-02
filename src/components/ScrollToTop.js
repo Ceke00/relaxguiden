6809f2e5b738b674916f27a48ff62d6f6017f14a
Cecilia Egevad <ceciliaegevad@gmail.com>
2026-01-02 16:10:30 +0100

    small cahnge

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -2,9 +2,11 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
-  const { pathname } = useLocation();
+  const { pathname, hash } = useLocation();
 
   useEffect(() => {
+    if (hash) return;
+
     window.scrollTo(0, 0);
   }, [pathname]);
 
